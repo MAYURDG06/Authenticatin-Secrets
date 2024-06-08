5ed15f9754b9d0d438dd88c426db2fd27a411c33
MAYURDG <mayurgoradiya8322@gmail.com>
2024-06-08 21:51:00 +0530

    Level-2 encryption

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyparser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-
+const encrypt = require("mongoose-encryption")
 
 const app = express()
 
@@ -12,10 +12,13 @@ app.use(bodyparser.urlencoded({extended: true}))
 
 mongoose.connect("mongodb://localhost:27017/userDB")
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-}
+})
+
+const secret = "Thisisourlittlesecret."
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema)
 
